01cbca1a48cc4f13866af63821a4091a75896f71
Huynh Thanh Nhan <huynhthanhnhan632004@gmail.com>
2024-03-11 11:31:07 +0700

    No change 11/3/2024

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,11 @@ const modal = $('.modal')
 const loginBtn = $('#header__navbar-item-login');
 const registerBtn = $('#header__navbar-item-Register')
 const returnShowbtn = $$('.auth-form__controls-back');
+const paginationbtn = $$('.pagination-item');
 
 // show log and  Register
+const showPaginationactive = $$('.pagination-item');
+console.log(showPaginationactive);
 const showRegister = $('.auth-form-Register');
 const showLogin = $('.auth-form-login');
 //   app
@@ -20,15 +23,24 @@ const app = {
         registerBtn.onclick = function () {
             modal.style.display = "flex";
             showRegister.style.display = "none";
-        }
+        },
 
-        returnShowbtn.forEach(function (item) {
-            item.addEventListener("click", function () {
-                modal.style.display = "none";
-                showLogin.style.display = "block";
-                showRegister.style.display = "block";
+            returnShowbtn.forEach(function (item) {
+                item.addEventListener("click", function () {
+                    modal.style.display = "none";
+                    showLogin.style.display = "block";
+                    showRegister.style.display = "block";
+                });
+            }),
+            paginationbtn.forEach(function (page) {
+                page.addEventListener('click', function (event) {
+                    event.preventDefault();
+                    paginationbtn.forEach(function (item) {
+                        item.classList.remove('pagination-item--active');
+                    });
+                    page.classList.add('pagination-item--active');
+                });
             });
-        });
     },
 
     start: function () {
@@ -37,3 +49,4 @@ const app = {
 }
 
 app.start();
+
